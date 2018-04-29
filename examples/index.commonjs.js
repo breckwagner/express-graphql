857ca8cb68a6ac643b1e5561f8fd9fbaeb72ae5e
Richard B. Wagner <breckwagner@gmail.com>
2018-04-29 12:33:36 -0400

    Added install to test npm for myself

---
+++ examples/index.commonjs.js
@@ -0,0 +1,33 @@
+/**
+ *  Copyright (c) 2015-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+const express = require('express');
+const graphqlHTTP = require('../dist/index.js');
+const buildSchema = require('graphql').buildSchema;
+
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+const root = {
+  hello: () => 'Hello world!',
+};
+
+const app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
